315d76a0556af6fdf892a683431927b09d7484d0
Matt Auerbach <tennisfan0526@gmail.com>
2019-04-17 15:53:07 +0100

    merge

--- backend/get_id.py
+++ backend/get_id.py
@@ -1,5 +1,4 @@
 # Stardew Dev Tour Demo
-
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
@@ -9,14 +8,18 @@ dynamodb = boto3.resource('dynamodb', 'us-east-2')
 table_users = dynamodb.Table('stardew_weather_state')
 
 def lambda_handler(event, context):
-    channel_id = event['queryStringParameters']['channel_id']
-    response_dynamo = table_users.get_item(Key={'channel_id': channel_id})
+   
+    try:
+        channel_id = event['queryStringParameters']['channel_id']
+        response_dynamo = table_users.get_item(Key={'channel_id': channel_id})
+    except ClientError as e:
+        print("There was an error: ", e)
 
     vote_id = str(response_dynamo['Item']['vote_id'])
-    resp = {
-        'vote_id': vote_id
-    }
+    resp = {'vote_id': vote_id}
+
     return {
+
         'statusCode': 200,
         'body': json.dumps(resp),
         'headers': {
--- backend/vote.py
+++ backend/vote.py
@@ -1,3 +1,4 @@
+# Stardew Dev Tour Demo
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
@@ -9,52 +10,57 @@ votes_table = dynamodb.Table('stardew_votes')
 viewers_table = dynamodb.Table('stardew_viewers')
 
 def lambda_handler(event, context):
-    
     return_message = 'Vote recorded'
-
     event_body = json.loads(event['body'])
-
     try: 
         user_id = event_body['user_id']
         channel_id = str(event_body['channel_id'])
         vote_id = str(event_body['vote_id'])
         vote_item = str(event_body['vote_item'])
     except:
-        print('error')
+        print('Error')
         return {
-        'statusCode': 500,
-        'body': json.dumps('Error')
+            'statusCode': 500,
+            'body': json.dumps('Error')
         }
-    
-    resp = viewers_table.update_item(
-        Key={"user_id": user_id, "channel_vote": channel_id + ":" + vote_id},
-        UpdateExpression="SET vote_item = :vote_item",
-        ExpressionAttributeValues={":vote_item": vote_item},
-        ReturnValues="UPDATED_OLD"
-    )
+    try:
+        resp = viewers_table.update_item(
+            Key={"user_id": user_id, "channel_vote": channel_id + ":" + vote_id},
+            UpdateExpression="SET vote_item = :vote_item",
+            ExpressionAttributeValues={":vote_item": vote_item},
+            ReturnValues="UPDATED_OLD"
+        )
+    except ClientError as e:
+        print("There was an error: ", e)
 
     previous_vote = resp.get('Attributes', {}).get('vote_item')
-    # check if user is voting with same previous vote
-    if previous_vote == vote_item:
-        return 
 
     # Decrement the previous vote if it existed
     if previous_vote:
-       update_response= votes_table.update_item(
-            Key={"channel_vote": channel_id + ":" + vote_id, 'vote_item': previous_vote},
-            UpdateExpression="ADD votes :incr",
-            ExpressionAttributeValues={":incr": -1},
-        )
-       return_message = 'Changed your vote'
-        
+        # check if user is voting with same previous vote
+        if previous_vote == vote_item:
+            return
+        try:
+            votes_table.update_item(
+                Key={"channel_vote": channel_id + ":" + vote_id, 'vote_item': previous_vote},
+                UpdateExpression="ADD votes :incr",
+                ExpressionAttributeValues={":incr": -1},
+            )
+        except ClientError as e:
+            print("There was an error: ", e)
+        return_message = 'Changed your vote'
+  
     # we always add the new vote in
-    resp = votes_table.update_item(
-        Key={'channel_vote': channel_id + ":" + vote_id, 'vote_item': vote_item},
-        UpdateExpression="ADD votes :incr",
-        ExpressionAttributeValues={":incr": 1},
-        ReturnValues="ALL_NEW"
-    )
-    
+    try:
+        votes_table.update_item(
+            Key={'channel_vote': channel_id + ":" + vote_id, 'vote_item': vote_item},
+            UpdateExpression="ADD votes :incr",
+            ExpressionAttributeValues={":incr": 1},
+            ReturnValues="ALL_NEW"
+        )
+    except ClientError as e:
+        print("There was an error: ", e)
+
     return {
         'statusCode': 200,
         'body': json.dumps(return_message),
